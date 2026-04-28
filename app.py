f4bb27c5b6fc38b5eeae415363e74b79276a2a2d
Arushi Tiwari <arushi.coding7@gmail.com>
2026-04-28 16:46:15 +0530

    Refactor lambda_handler to improve security

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
- password = "12345" # Vulnerability
- return {
- 'statusCode': 200,
- 'body': 'Hello from Secure CI/CD Pipeline!'
- }
+    password = "12345"  # Vulnerability for testing
+    return {
+        'statusCode': 200,
+        'body': 'Hello Secure CI/CD'
+    }
